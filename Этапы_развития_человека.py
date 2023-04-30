a9482af4432133bbe2c9447c7708101d92d873cf
moiseesya <121014855+moiseesya@users.noreply.github.com>
2023-04-30 20:57:22 +1000

    Add files via upload

---
+++ Этапы_развития_человека.py
@@ -0,0 +1,8 @@
+a=input ('Stage 1 of human development:')
+b=input ('Stage 2 of human development:')
+c=input ('Stage 3 of human development:')
+d=input ('Stage 4 of human development:')
+e=input ('Stage 5 of human development:')
+f=input ('Stage 6 of human development:')
+g=input ('Stage 7 of human development:')
+print (a,b,c,d,e,f,g, sep='=> ')
